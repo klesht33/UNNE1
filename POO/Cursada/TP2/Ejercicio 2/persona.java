61f7a14ae6a1934416257eae28ebfc261127405f
klesht33 <mesmero771@gmail.com>
2026-08-11 15:38:58 -0300

    vault backup: 2026-08-11 15:38:58

---
+++ POO/Cursada/TP2/Ejercicio 2/persona.java
@@ -0,0 +1,68 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class persona {
+
+
+private int dni;
+private String nombre;
+private String apellido;
+private int anioNacimiento;
+
+public persona(int dni, String nombre, String apellido, int anioNacimiento) {
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.anioNacimiento = anioNacimiento;
+}
+
+private void setDNI(int dni) {
+    this.dni = dni;
+}
+
+private void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+private void setApellido(String apellido) {
+    this.apellido = apellido;
+}
+
+private void setAnioNacimiento(int anioNacimiento) {
+    this.anioNacimiento = anioNacimiento;
+}
+
+public int getDNI() {
+    return dni;
+}
+
+public String getNombre() {
+    return nombre;
+}
+
+public String getApellido() {
+    return apellido;
+}
+
+public int getAnioNacimiento() {
+    return anioNacimiento;
+}
+
+public int edad() {
+    Calendar fechaHoy = new GregorianCalendar();
+    int anioHoy = fechaHoy.get(Calendar.YEAR);
+
+    return anioHoy - anioNacimiento;
+}
+
+public String nomYApe() {
+    return nombre + " " + apellido;
+}
+
+public void mostrar() {
+    System.out.println("Nombre y Apellido: " + nomYApe());
+    System.out.println("DNI: " + getDNI() + " Edad: " + edad() + " años: " + getAnioNacimiento());
+}
+
+
+}
---
+++ POO/Cursada/TP2/Ejercicio 2/personaMain.java
@@ -0,0 +1,8 @@
+
+
+public class personaMain {
+    public static void main(String[] args) {
+        persona p = new persona(35123456, "Jaaaaauan", "Pérez", 2001);
+        p.mostrar();
+    }
+}
