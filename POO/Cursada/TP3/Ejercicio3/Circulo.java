38bdb779b2ea16cbe02ea5cd4e7abfafa4addb9b
luismabonessi <bonessiluismaria5@gmail.com>
2026-08-23 20:00:07 -0300

    Add files via upload

---
+++ POO/Cursada/TP3/Ejercicio3/Circulo.java
@@ -0,0 +1,64 @@
+public class Circulo {
+    private double radio;
+    private punto centro;
+    
+    public Circulo(double pRadio, punto pCentro){
+        this.setRadio(pRadio);
+        this.setCentro(pCentro);
+        this.radio = pRadio;
+        this.centro = pCentro;
+    }
+    
+    public Circulo(){
+        this.radio = 0;
+        this.centro = new punto (0, 0);
+    }
+    
+    private void setRadio(double pRadio){   
+        this.radio = pRadio;
+
+    }
+    
+    private void setCentro(punto pCentro){
+        this.centro = pCentro;
+    }
+    
+    public double getRadio(){
+        return radio;
+    }
+    
+    public punto getCentro(){
+        return centro;
+    }
+    
+    public void desplazar(double pdx, double pdy){
+        this.centro.desplazar(pdx, pdy);
+    }
+    
+    public double perimetro(){
+        return 2 * Math.PI * getRadio();
+    }
+    
+    public double superficie (){
+        return Math.PI * getRadio() * getRadio();
+    }
+    
+    public double distanciaA(Circulo otroCirculo){
+        return this.centro.distanciaA(otroCirculo.getCentro());
+    }
+    
+    public Circulo elMayor(Circulo otroCirculo){
+        if(this.superficie() > otroCirculo.superficie()){
+            return this;
+        }
+            else {
+                return otroCirculo;
+            }
+    }
+    
+      public void caracteristicas() {
+        System.out.println("****** Circulo ******");
+        System.out.println("Centro: " + centro.coordenadas() + " - Radio: " + radio);
+        System.out.println("Superficie: " + superficie() +  " - Perimetro: " + perimetro());
+    }
+}
---
+++ POO/Cursada/TP3/Ejercicio4/Rectangulo.java
@@ -0,0 +1,70 @@
+public class Rectangulo{
+    private punto origen;
+    private double ancho;
+    private double alto;
+
+    public Rectangulo(punto pOrigen, double pAncho, double pAlto){
+        this.setAncho(pAncho);
+        this.setAlto(pAlto);
+        this.setOrigen(pOrigen);
+    }
+    
+    public Rectangulo(double pAncho, double pAlto){
+        this.setAncho(pAncho);
+        this.setAlto(pAlto);
+    }
+    
+    private void setAncho(double pAncho){
+        this.ancho = pAncho;
+    }
+    
+    private void setAlto(double pAlto){
+        this.alto = pAlto;
+    }
+    
+    private void setOrigen(punto pOrigen){
+        this.origen = pOrigen;
+    }
+    public double getAncho(){
+        return this.ancho; 
+    }
+    
+    public double getAlto(){
+        return this.alto;
+    }
+    
+    public punto getOrigen(){
+        return this.origen;
+    }
+    
+    public void desplazar(double pDx,  double pDy){
+        this.origen.desplazar(pDx, pDy);
+    }
+    
+    public double perimetro(){
+        return 2 * getAncho() + 2 * getAlto();
+    }
+    
+    public double superficie(){
+        return getAncho() * getAlto();    
+    }
+    
+    public double distanciaA(Rectangulo otroRectangulo){
+        return this.origen.distanciaA(otroRectangulo.origen);
+    }
+    
+    public Rectangulo elMayor(Rectangulo otroRectangulo){
+        if(this.superficie() > otroRectangulo.superficie()){
+            return this;
+        }
+        else{
+            return otroRectangulo;
+        }
+    }
+    
+    public void mostrarDatos(){
+        System.out.println("***Rectangulo***");
+        System.out.println("Origen: "+ origen.coordenadas() + "- Alto" + getAlto() + "-Ancho" + getAncho());
+        System.out.println("Superficie: "+ superficie() + "-Perimetro: "+ perimetro() );
+    }
+}
